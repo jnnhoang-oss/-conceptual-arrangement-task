e3e54c4892d589f6ae886f5dd75214c47b7f79c0
Jennifer Hoang <jnnhoang@bu.edu>
2025-11-09 19:15:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,14 @@ let arenaVisible = false;
 const imageFolder = ".github/wth/";
 const imageFiles = [
   "aardvark.jpg","anteater.jpg","brown_bear.jpg","camel.jpg","canary.jpg",
-  "carp.jpg","catfish.jpg","chipmunk.jpg","elephantafrican.jpg","finch.jpg"
+    "carp.jpg","caterpillarhawkmoth.jpg","catfish.jpg","chipmunk.jpg","cranebug.jpg",
+    "cricket.jpg","elephantafrican.jpg","finch.jpg","firebug.jpg","flea.jpg",
+    "gerbil.jpg","giraffe.jpg","goldfish.jpg","halibut.jpg","herculesbeetle.jpg",
+    "herring.jpg","horse.jpg","hyena.jpg","leopard.jpg","llama.jpg","marmot.jpg",
+    "mouse.jpg","ostrich.jpg","palmcockatoo.jpg","partridge.jpg","pelican.jpg",
+    "perch.jpg","pigeon.jpg","pike.jpg","porcupine.jpg","prayingmantis.jpg",
+    "rabbit.jpg","reindeer.jpg","salmon.jpg","shark.jpg","sheep.jpg","shrimp.jpg",
+    "skunk.jpg","snail.jpg","starfish.jpg","tiger.jpg","turkey.jpg","turkey copy.jpg","waterbuffalo.jpg"
 ];
 
 // --- Load and display images ---
