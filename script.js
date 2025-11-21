365dc23d8fad8f87bfa273ebb9a0d608fa9e66db
Jennifer Hoang <jnnhoang@bu.edu>
2025-11-21 10:46:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const arenaContainer = document.getElementById("arenaContainer");
 const arena = document.getElementById("arena");
 const instructions = document.getElementById("instructions");
+const fullscreen = document.getElementById("fullscreen");
 const questions = document.getElementById("questions");
 const endScreen = document.getElementById("endScreen");
 const totalTimeDisplay = document.getElementById("totalTime");
